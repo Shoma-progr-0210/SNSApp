49ba60c422b88a879a3fd4c3fd5ed1e2608731f8
Shoma-progr-0210 <Shoma.progr.0210@gmail.com>
2020-06-26 01:28:20 +0900

    fix:クラスにjavadocを記入

--- SNSApp/src/action/TimeLineAction.java
+++ SNSApp/src/action/TimeLineAction.java
@@ -1,12 +1,24 @@
 package action;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 
+import entity.MessageEntity;
+import service.PerformanceService;
 import util.DBAccessException;
 import util.DataInconsistencyException;
 import util.ParamCheckException;
 
+/**
+ * タイムラインを表示するアクションクラス
+ *
+ * @author shoma
+ *
+ */
 public class TimeLineAction extends PerformanceAction {
+
+    private final PerformanceService Perservice = new PerformanceService();
 
     @Override
     protected String processPerformanceManagement(HttpServletRequest request)
@@ -14,6 +26,12 @@ public class TimeLineAction extends PerformanceAction {
 
         //セッション継続
         request.getSession(false);
+
+        //タイムライン情報を取得
+        ArrayList<MessageEntity> messageEntityList = Perservice.selectAllMessageInfo();
+
+        //リクエストにメッセージオブジェクトリストをセット
+        request.setAttribute("messageEntityList", messageEntityList);
 
         return "/jsp/timeLine.jsp";
     }
