d9fa9ce3c3adaab6f6a123e7da50f9af9ce1efa5
Shoma-progr-0210 <Shoma.progr.0210@gmail.com>
2020-06-27 18:37:24 +0900

    fix:エラー情報テーブルのテーブル名を修正

--- SNSAppSQL.sql
+++ SNSAppSQL.sql
@@ -20,7 +20,7 @@ ERROR_CD CHAR(4) primary key,
 ERROR_WORDS varchar(100) not null
 );
 
-create table MESSAGE(
+create table MESSAGE_INFO(
 MSG_NO int(6) UNSIGNED ZEROFILL primary key AUTO_INCREMENT,
 USER_NO char(12),
 MESSAGE varchar(200),
@@ -33,13 +33,23 @@ foreign key (USER_NO) references USER_INFO(USER_NO)
 
 insert into USER_MASTER values('tester', 'testtest');
 insert into USER_INFO values('tester', 'FamilyName', 'FirstName', '20200627','testSNSApp');
-insert into message(USER_NO, MESSAGE, DEL_FLG, CREATE_DT, UPDATE_DT)
+insert into message_INFO(USER_NO, MESSAGE, DEL_FLG, CREATE_DT, UPDATE_DT)
  values('tester','Hello, This is SNSApp.', 0, LOCALTIMESTAMP(), LOCALTIMESTAMP());
  
+insert into error_info values('1001','DBアクセスエラーです。');
+insert into error_info values('0105','該当するユーザが存在しません。ログインに失敗しました。');
+insert into error_info values('0106','ユーザIDは5桁以上12桁以下で入力してください。');
+insert into error_info values('0107','ユーザIDは半角英数字で入力してください。');
+insert into error_info values('0103','パスワードは8桁以上16桁以下で入力してください。');
+insert into error_info values('0104','パスワードは半角英数字で入力してください。');
+insert into error_info values('0601','セッションが切断されました。もう一度ログインからやり直してください。');
+insert into error_info values('0701','メッセージは200文字以下で入力して下さい。');
+insert into error_info values('0702','メッセージが送信出来ませんでした。');
+
+
 select * from user_master;
 select * from user_info;
-select * from message;
+select * from message_INFO;
  
-
 
 
